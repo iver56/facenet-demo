3ffbb2838cc4f2c870e49ea0110ef0a193377576
iver56 <iver56@hotmail.com>
2018-02-18 13:32:43 +0100

    Serve index.html through flask, for simplicity

--- classifier_service.py
+++ classifier_service.py
@@ -2,7 +2,12 @@ from flask import Flask
 from flask import jsonify
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='')
+
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
 
 
 @app.route("/classify/", methods=['POST'])
