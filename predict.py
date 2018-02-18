86cc44ae8567187af153c8b20c54c5c33e709960
iver56 <iver56@hotmail.com>
2018-02-18 21:14:09 +0100

    Decode prediction names properly, and don't load the labels file for each prediction

--- predict.py
+++ predict.py
@@ -1,7 +1,9 @@
 import numpy as np
 from keras.preprocessing import image
-from keras_vggface.vggface import VGGFace
+from keras.utils.data_utils import get_file
 from keras_vggface import utils
+from keras_vggface.utils import V2_LABELS_PATH, VGGFACE_DIR
+from keras_vggface.vggface import VGGFace
 
 from helpers import base64_png_image_to_pillow_image
 
@@ -11,6 +13,8 @@ class Classifier:
 
     def __init__(self):
         self.model = VGGFace(model='resnet50')
+        file_path = get_file('rcmalli_vggface_labels_v2.npy', V2_LABELS_PATH, cache_subdir=VGGFACE_DIR)
+        self.labels = np.load(file_path)
 
     def predict(self, base64_png):
         # Convert base64 to Pillow image instance
@@ -23,11 +27,22 @@ class Classifier:
         that_image = that_image.convert('RGB')
 
         #that_image = image.load_img('./image/ajb.jpg', target_size=(224, 224))
+        
         # Convert the PIL image to a numpy array
         x = image.img_to_array(that_image)
         x = np.expand_dims(x, axis=0)
         x = utils.preprocess_input(x, version=2)  # version=2 is valid for resnet50
         predictions = self.model.predict(x)
-        decoded_predictions = utils.decode_predictions(predictions)
+        decoded_predictions = self.decode_predictions(predictions)
+        decoded_prediction = decoded_predictions[0]
 
-        return decoded_predictions
+        return decoded_prediction
+
+    def decode_predictions(self, predictions, top=5):
+        results = []
+        for prediction in predictions:
+            top_indices = prediction.argsort()[-top:][::-1]
+            result = [[self.labels[i], prediction[i]] for i in top_indices]
+            result.sort(key=lambda x: x[1], reverse=True)
+            results.append(result)
+        return results
