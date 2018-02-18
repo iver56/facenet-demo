8974e01607d371b443ab4cf136d983baaf76f730
iver56 <iver56@hotmail.com>
2018-02-18 13:21:40 +0100

    Correct input preprocessing

--- predict.py
+++ predict.py
@@ -8,6 +8,6 @@ model = VGGFace(model='resnet50')
 img = image.load_img('./image/ajb.jpg', target_size=(224, 224))
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
-x = utils.preprocess_input(x, version=1) # or version=2
+x = utils.preprocess_input(x, version=2)  # version=2 is valid for resnet50
 preds = model.predict(x)
 print('Predicted:', utils.decode_predictions(preds))
