ca99984a177ef0083f103d6a616cd348a7a6a3f2
iver56 <iver56@hotmail.com>
2018-02-18 13:12:42 +0100

    Add prediction script

---
+++ predict.py
@@ -0,0 +1,13 @@
+import numpy as np
+from keras.preprocessing import image
+from keras_vggface.vggface import VGGFace
+from keras_vggface import utils
+
+model = VGGFace(model='resnet50')
+
+img = image.load_img('./image/ajb.jpg', target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = utils.preprocess_input(x, version=1) # or version=2
+preds = model.predict(x)
+print('Predicted:', utils.decode_predictions(preds))
