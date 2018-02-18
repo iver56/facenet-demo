4e5a313ad268a7890375095e1d698bd9d82f439d
iver56 <iver56@hotmail.com>
2018-02-18 13:52:26 +0100

    Move static web code to separate folder. Add jQuery.

--- web_service.py
+++ web_service.py
@@ -1,8 +1,7 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask import jsonify
 
-
-app = Flask(__name__, static_folder='')
+app = Flask(__name__, static_folder='web_app')
 
 
 @app.route('/')
@@ -17,8 +16,14 @@ def classify():
     })
 
 
+@app.route('/<path:path>')
+def static_proxy(path):
+    # send_static_file will guess the correct MIME type
+    return app.send_static_file(path)
+
+
 if __name__ == "__main__":
     app.run(
         host='0.0.0.0',
-        debug=False
+        debug=True
     )
